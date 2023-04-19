293dba8c04dc3f0a564547018eb5bddd5c679a3e
Sehajpreet Singh <sehajmagan@gmail.com>
2023-04-18 20:57:44 -0400

    updated code for to do application

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ToDoList from "./components/toDoList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Neo Brutalist Shopping List</h1>
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
--- src/components/toDoList/index.jsx
+++ src/components/toDoList/index.jsx
@@ -1,31 +1,46 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "./TodoList.css";
+
 const ToDoList = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos((todos) => [...todos, newTodo]);
+  };
   return (
     <div className="container">
-      <form>
-        <div className="field">
-          <div className="control">
-            <input
-              className="input"
-              type="text"
-              placeholder="Add a new to-do item"
-              value={newTodo}
-              onChange={(e) => setNewTodo(e.target.value)}
-            />
-          </div>
-        </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          placeholder="Add a new to-do item"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
         <button className="button is-primary" type="submit">
           Add
         </button>
       </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+      <motion.ul
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        {todos.map((todo, index) => (
+          <motion.li
+            key={index}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {todo}
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </div>
   );
 };
 
-export default TodoList;
+export default ToDoList;
